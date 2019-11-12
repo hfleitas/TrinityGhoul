a9ef76fa16bdd900c8e5768a2219f8ff07bd6f23
Hiram Fleitas <hiramfleitas@hotmail.com>
2019-11-12 14:27:41 -0500

    Update Cleanup.sql

--- Cleanup.sql
+++ Cleanup.sql
@@ -2,15 +2,15 @@
 
 --Keep Files <14 days old.
 --Delete Files from 14 - 29 days old.
-EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfs\sqlshared\prodbackups\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-14) -and $_.LastWriteTime -gt (Get-Date).AddDays(-29)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
+EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfs\prodbackups\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-14) -and $_.LastWriteTime -gt (Get-Date).AddDays(-29)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
 GO
 
 --Keep Files 30 days old.
 --Delete Files from 31 - 59 days old.
-EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfs\sqlshared\prodbackups\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-31) -and $_.LastWriteTime -gt (Get-Date).AddDays(-59)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
+EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfs\prodbackups\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-31) -and $_.LastWriteTime -gt (Get-Date).AddDays(-59)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
 GO
 
 --Keep Files 60 days old.
 --Delete Files >= 61 days old.
-EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfs\sqlshared\prodbackups\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-61)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
+EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfs\prodbackups\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-61)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
 GO
