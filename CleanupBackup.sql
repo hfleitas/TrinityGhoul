ac2afb7ad69a00ca345329062cb6dccc8a3ff418
Hiram Fleitas <hiramfleitas@hotmail.com>
2019-11-12 14:28:16 -0500

    Update CleanupBackup.sql

--- CleanupBackup.sql
+++ CleanupBackup.sql
@@ -1,4 +1,4 @@
 -- Add as last step to maintenance plan.
 --Delete Files >= 5 days old.
-EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfs\sqlshared\prodbackups\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-5)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
+EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfs\prodbackups\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-5)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
 GO
