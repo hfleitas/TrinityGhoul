d1ac6d237a6db4a5ac80d81c2b8ec768f7b133df
Hiram Fleitas <hiramfleitas@hotmail.com>
2019-11-12 14:07:08 -0500

    Create CleanupArchive.sql

---
+++ CleanupArchive.sql
@@ -0,0 +1,16 @@
+--Add as finishing step in maintenance plan.
+
+--Keep Files <14 days old.
+--Delete Files from 14 - 29 days old.
+EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfsarchive\prodarchive\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-14) -and $_.LastWriteTime -gt (Get-Date).AddDays(-29)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
+--GO
+
+--Keep Files 30 days old.
+--Delete Files from 31 - 59 days old.
+EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\\nfsarchive\prodarchive\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-31) -and $_.LastWriteTime -gt (Get-Date).AddDays(-59)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
+--GO
+
+--Keep Files 60 days old.
+--Delete Files >= 61 days old.
+EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfsarchive\prodarchive\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-61)} | Foreach { Remove-Item -Force -Verbose $_.fullname }" '
+GO
