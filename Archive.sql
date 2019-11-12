15a81cf95c59d5b1f0300069fd71c24cd7ba0b67
Hiram Fleitas <hiramfleitas@hotmail.com>
2019-11-12 14:22:51 -0500

    Create Archive.sql

---
+++ Archive.sql
@@ -0,0 +1,4 @@
+-- Add before last step of maint plan.
+--Archive *.bak(s) older than 3 days.
+EXECUTE master.dbo.xp_cmdshell '@PowerShell -noprofile -command "gci ''\\nfsarchive\prodarchive\cluster1\*.bak'' | Where {$_.LastWriteTime -lt (Get-Date).AddDays(-3)} | Move-Item -destination ''\\nfsarchive\prodarchive\cluster1\'' -Force -Verbose" '
+GO
